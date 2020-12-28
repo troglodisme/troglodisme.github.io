08c364628891acb4c8375f905d2cd311c713de93
troglodisme <giulio.ammendola@network.rca.ac.uk>
2020-12-28 09:18:30 +0000

    .

--- scripts/slides.js
+++ scripts/slides.js
@@ -3,17 +3,33 @@ window.addEventListener("keyup", function(e){if(e.keyCode == 27)history.back();}
 
 //TO DO: add something to stop the browswer go to empty page in case there is no previous history
 
-// pick all of the images and layer them based on the z-index
+const counter = document.getElementById("counter")
+
+window.onload = function() {
+  updateCounter();
+};
+
 const slideArea = document.querySelector("div.slides")
 const images = slideArea.querySelectorAll("img")
 
-// we want to keep track of two things
 let currentSlide = 0
-let z = 1
+let z = 0
 
 // when i click the slide area, change the slide based on z-index
 slideArea.addEventListener("click", function () {
-  currentSlide = currentSlide + 1
+
+  next();
+  updateCounter();
+
+})
+
+const updateCounter = function(){
+    console.log(currentSlide + 1);
+    counter.innerHTML = (currentSlide + 1 ) + "/" + images.length;
+}
+
+const next = function(){
+  currentSlide = currentSlide + 1;
 
   if (currentSlide > images.length - 1) {
     currentSlide = 0
@@ -26,7 +42,7 @@ slideArea.addEventListener("click", function () {
     image.style.animation = ""
   })
 
-  // pick the right image
+  // // pick the right image
   images[currentSlide].style.zIndex = z
   images[currentSlide].style.animation = "fade 0.5s"
-})
+}
